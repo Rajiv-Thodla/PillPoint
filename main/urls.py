8618c5cf36b482ef8435a2e14f94eca83605a1d1
Rajiv Thodla <rajivthodla@gmail.com>
2025-02-08 01:58:57 +0530

    renamed and modified files

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path("<int:id>" , views.function1 , name = "function1"),
-
+    path("function1/<int:id>/", views.function1, name="function1"),  # Prefixed path
 ]
